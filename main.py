df590a16f430386e8f24b9cf2db215f380d57f00
Maxence BEUNEL <maxence.beunel@epita.fr>
2023-10-17 21:53:48 +0200

    répare ta merde connard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scraper.scraperSofa import get_id, get_odds
-from Winner.getWinner import calculate_team_to_bet
+from winner.getWinner import calculate_team_to_bet
 
 odds_info = get_odds(get_id("all_id"), "json_id")
 calculate_team_to_bet()
