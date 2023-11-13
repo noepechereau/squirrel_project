d39f76587f2bd8fd242c3eab9a40d300ce076a4b
noepechereau <noe.pechereau@epita.fr>
2023-11-13 18:20:57 +0000

    feat: all trad

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from spreadsheet.fetch import fetch
 from winner.get_winner import calculate_team_to_bet
 
 get_odds(get_id("all_id"), "json_id")
-matches = fetch(calculate_team_to_bet())
-[print(m) for m in matches]
+#matches = fetch(calculate_team_to_bet())
+#[print(m) for m in matches]
